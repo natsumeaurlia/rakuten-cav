05f670293681f8aee8ce2893d974438755891394
natsumeaurlia <moto718program@gmail.com>
2023-04-13 05:30:35 +0900

    update and fix

--- src/util.ts
+++ src/util.ts
@@ -1,35 +1,48 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { parse } from 'csv-parse/sync';
-import type { PaymentResult, PaymentTotalKey, Record } from './types';
+import * as fs from 'fs'
+import * as path from 'path'
+import { parse } from 'csv-parse/sync'
+import type { PaymentResult, PaymentTotalKey, Record } from './types'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
-export async function sumTotalPayment(downloadDir: string) {
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Tokyo')
+
+export async function sumTotalPaymentDir(downloadDir: string) {
   // ファイル一覧を取得
-  const files = fs.readdirSync(downloadDir);
-  const result: PaymentResult[] = [];
+  const files = fs.readdirSync(downloadDir)
+  const result: PaymentResult[] = []
   for (const file of files) {
     // 拡張子がcsvのファイルのみ処理する
     if (path.extname(file) === '.csv') {
-      const csvData = fs.readFileSync(path.join(downloadDir, file), { encoding: 'utf8' });
-      // BOMを削除する
-      const csvDataWithoutBom = csvData.replace(/^\uFEFF/, '');
-      const records: Record[] = parse(csvDataWithoutBom, {
-        columns: true,
-        skip_empty_lines: true,
-      });
-      let fileResult: PaymentResult = {};
-      for await (const record of records) {
-        const paymentMonth = record['支払月']
-        const totalKey: PaymentTotalKey = `${paymentMonth}支払金額`
-        // x月支払金額
-        const payment = parseFloat(record[totalKey] || '0');
-        
-        if (!isNaN(payment)) {
-          fileResult[totalKey] = (fileResult[totalKey] || 0) + payment;
-        }
-      }
-      result.push(fileResult);
+      const fileResult = await sumTotalPayment(path.join(downloadDir, file))
+      result.push(fileResult)
     }
   }
-  return result;
+  return result
+}
+
+export async function sumTotalPayment(csvFilePath: string) {
+  // CSVファイルを読み込む
+  const csvData = fs.readFileSync(csvFilePath, { encoding: 'utf8' })
+  // BOMを削除する
+  const csvDataWithoutBom = csvData.replace(/^\uFEFF/, '')
+  const records: Record[] = parse(csvDataWithoutBom, {
+    columns: true,
+    skip_empty_lines: true,
+  })
+  const result: PaymentResult = {}
+  for await (const record of records) {
+    const paymentMonth = record['支払月'] || dayjs().month() + 1 + '月'
+    const totalKey: PaymentTotalKey = `${paymentMonth}支払金額`
+    // x月支払金額
+    const payment = parseFloat(record[totalKey] || '0')
+
+    if (!isNaN(payment)) {
+      result[totalKey] = (result[totalKey] || 0) + payment
+    }
+  }
+  return result
 }
